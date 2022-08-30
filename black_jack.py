e91bd9fdc76d063d2a8298190071ceb38dfeafe8
csroland <112256621+csroland@users.noreply.github.com>
2022-08-30 18:44:55 +0200

    Updated BlackJack, started working on Telegram bot

--- black_jack.py
+++ black_jack.py
@@ -8,17 +8,27 @@ class Player:
         self.finished = False
 
 class BlackJackGame:
-    def __init__(self, deck: CardDeck, player: Player):
+    def __init__(self, deck: CardDeck, player: Player, bot):
         self.deck = deck
         self.player = player
         self.dealer = Player("Dealer", 0)
+        self.bot = bot
+
 
     def black_jack_round(self) -> None:
-        input("New round started")
+        bet_success = False
+        while not bet_success:
+            bet = int(input("Place your bet: "))
+            if bet <= self.player.money and bet > 0:
+                self.player.money -= bet
+                bet_success = True
+            else:
+                print("can't do")
         self.player.cards.extend(self.deck.draw(1))
         self.dealer.cards.extend(self.deck.draw(1))
         self.player.cards.extend(self.deck.draw(1))
         self.dealer.cards.extend(self.deck.draw(1))
+
         while True:
             hand_value = self.bj_hand_value(self.player.cards)
             dealer_value = self.bj_hand_value(self.dealer.cards)
@@ -27,18 +37,16 @@ class BlackJackGame:
             if not self.player.finished:
                 if dealer_value == 21:
                     if hand_value == 21:
-                        print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", hand_value)
-                        print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+                        self.print_stats()
                         print("Stalemate")
+                        self.player.money += bet
                         return
                     else:
-                        print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", hand_value)
-                        print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+                        self.print_stats()
                         print("You lose")
                         return
                 elif hand_value == 21:
-                    print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", hand_value)
-                    print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+                    self.print_stats()
                     print("You Won!")
                     return
                 elif hand_value < 21:
@@ -55,31 +63,35 @@ class BlackJackGame:
                     return
             else:
                 break
+
         while self.bj_hand_value(self.dealer.cards) < 17:
             self.dealer.cards.extend(self.deck.draw(1))
-            print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", hand_value)
-            print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+            self.print_stats()
         dealer_value = self.bj_hand_value(self.dealer.cards)
         if dealer_value > 21:
-            print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", hand_value)
-            print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+            self.print_stats()
             print("Dealer bust!")
+            self.player.money += 2*bet
             return
         if dealer_value > hand_value:
-            print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", hand_value)
-            print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+            self.print_stats()
             print("Dealer won!")
             return
         elif dealer_value == hand_value:
-            print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", hand_value)
-            print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+            self.print_stats()
             print("Stalemate!")
             return
         else:
             print("You won!")
-            print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", hand_value)
-            print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+            self.print_stats()
+            self.player.money += 2*bet
             return
+
+
+    def print_stats(self):
+        print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", self.bj_hand_value(self.player.cards))
+        print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
+
 
     def reset_table(self) -> None:
         self.deck.out.extend(self.player.cards)
@@ -87,9 +99,11 @@ class BlackJackGame:
         self.player.cards, self.dealer.cards = [], []
         self.player.finished = False
 
-    def shuffle_deck(self) -> None:
-        if len(self.deck.deck) < 65:
+
+    def shuffle_deck(self, thres: int) -> None:
+        if len(self.deck.deck) < thres:
             self.deck.shuffle_deck()
+
 
     def bj_hand_value(self, cards: list[str]) -> int:
         value = 0
@@ -107,7 +121,17 @@ class BlackJackGame:
                     value += 1
         return value
 
-    def game_loop(self):
+
+    def play_again(self) -> bool:
+        print("Your balance: ", self.player.money)
+        if input("want to play another round?: [y]es") == "y":
+            return True
+        else:
+            return False
+
+
+    def game_loop(self) -> bool:
         self.black_jack_round()
         self.reset_table()
-        self.shuffle_deck()
+        self.shuffle_deck(65)
+        return self.play_again()
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from black_jack import *
+import telebot
+from dotenv import load_dotenv
 import os
 
 
-def main():
+load_dotenv(".env")
+bot = telebot.TeleBot(os.environ.get("TOKEN"), parse_mode="HTML")
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome to BlackJack Bot! Type /play to start playing!")
+
+@bot.message_handler(commands=["play"])
+def start_black_jack(message):
     deck = CardDeck(FRENCH, 6)
     deck.shuffle_deck()
-    player = Player("Roland", 100)
-    game = BlackJackGame(deck, player)
-    while True:
-        game.game_loop()
+    player = Player(str(message.from_user.id), 1000)
+    game = BlackJackGame(deck, player, bot)
+    cont = True
+    while cont == True:
+        cont = game.game_loop()
+
+def main():
+    bot.infinity_polling()
+    # deck.shuffle_deck()
+    # player = Player("Roland", 1000)
+    # game = BlackJackGame(deck, player)
+    # cont = True
+    # while cont == True:
+    #     cont = game.game_loop()
 
 if __name__ == "__main__":
     main()
