5098ee84528fdd6639add547da7aace4f968f57c
csroland <112256621+csroland@users.noreply.github.com>
2022-09-01 01:25:54 +0200

    Rewritten part of the game logic
    Rewritten the game logic so it works with the continous polling of pyTelegramBotAPI. There are a few issues: currently the game only advances if any kind of data is received by the bot, "doubling" the bet is not yet implemented.

--- black_jack.py
+++ black_jack.py
@@ -1,4 +1,12 @@
 from cards import *
+from telebot import types
+
+#Define the markups used for the custom keyboards for the game:
+inline_btn_double = types.InlineKeyboardButton("Double", callback_data = "double")
+inline_btn_hit = types.InlineKeyboardButton("Hit", callback_data = "hit")
+inline_btn_pass = types.InlineKeyboardButton("Pass", callback_data = "pass")
+first_round_markup = types.InlineKeyboardMarkup([[inline_btn_double, inline_btn_hit, inline_btn_pass]])
+other_round_markup = types.InlineKeyboardMarkup([[inline_btn_hit, inline_btn_pass]])
 
 class Player:
     def __init__(self, name: str, money: int):
@@ -8,92 +16,138 @@ class Player:
         self.finished = False
 
 class BlackJackGame:
-    def __init__(self, deck: CardDeck, player: Player, bot):
-        self.deck = deck
-        self.player = player
+    def __init__(self, player_id: str, bot):
+        """
+        Starts the game by initializing the deck, the player and the dealer, starting in 'BET' state
+        """
+        self.deck = CardDeck(FRENCH, 6)
+        self.deck.shuffle_deck()
+        self.player_id = player_id
+        self.player = Player(self.player_id, 1000) #placeholder money
         self.dealer = Player("Dealer", 0)
         self.bot = bot
+        self.game_message_id = None
+        self.game_state = "BET"
 
+    def start_black_jack_round(self) -> None:
+        """
+        Starts the game after the bet was succesful and saves the game_message_id so the message can be later edited after user action
+        """
+        for i in range(2):
+            self.player.cards.extend(self.deck.draw(1))
+            self.dealer.cards.extend(self.deck.draw(1))
+        first_msg_string = "".join(["<b>Dealer:</b>\n", self.dealer.cards[0], " ??\n\n", "<b>Your cards:</b>\n", self.player.cards[0], " ", self.player.cards[1]])
+        self.game_message_id = self.bot.send_message(self.player_id, first_msg_string, reply_markup = first_round_markup).message_id
 
-    def black_jack_round(self) -> None:
-        bet_success = False
-        while not bet_success:
-            bet = int(input("Place your bet: "))
-            if bet <= self.player.money and bet > 0:
-                self.player.money -= bet
-                bet_success = True
-            else:
-                print("can't do")
-        self.player.cards.extend(self.deck.draw(1))
-        self.dealer.cards.extend(self.deck.draw(1))
-        self.player.cards.extend(self.deck.draw(1))
-        self.dealer.cards.extend(self.deck.draw(1))
-
-        while True:
-            hand_value = self.bj_hand_value(self.player.cards)
-            dealer_value = self.bj_hand_value(self.dealer.cards)
-            print(self.player.name, "cards: \t\t", self.player.cards, "\t\t\t value:", hand_value)
-            print(self.dealer.name, "cards: \t\t", [self.dealer.cards[0], "?"])
-            if not self.player.finished:
-                if dealer_value == 21:
-                    if hand_value == 21:
-                        self.print_stats()
-                        print("Stalemate")
-                        self.player.money += bet
-                        return
-                    else:
-                        self.print_stats()
-                        print("You lose")
-                        return
-                elif hand_value == 21:
+    def update_black_jack_round(self, action: str) -> None:
+        """
+        The main logic of the game, it updates the state of the game after user actions
+        """
+        hand_value = self.bj_hand_value(self.player.cards)
+        dealer_value = self.bj_hand_value(self.dealer.cards)
+        print(self.player.name, "cards: \t\t", self.player.cards, "\t\t\t value:", hand_value)
+        print(self.dealer.name, "cards: \t\t", [self.dealer.cards[0], "?"])
+        if not self.player.finished:
+            if dealer_value == 21:
+                self.dealer.finished = True
+                self.player.finished = True
+                if hand_value == 21:
                     self.print_stats()
-                    print("You Won!")
+                    print("Stalemate")
+                    self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
+                    self.player.money += bet
                     return
-                elif hand_value < 21:
-                    match input("[s]tand, or [h]it"):
-                        case "h":
-                            self.player.cards.extend(self.deck.draw(1))
-                        case "s":
-                            self.player.finished = True
-                            break
-                        case _:
-                            print("unkown")
-                elif hand_value > 21:
-                    print("Bust!")
+                else:
+                    self.print_stats()
+                    self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
+                    print("You lose")
                     return
-            else:
-                break
-
+            elif hand_value == 21:
+                self.player.finished = True
+                self.dealer.finished = True
+                self.print_stats()
+                self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
+                print("You Won!")
+                return
+            elif hand_value < 21:
+                match action:
+                    case "hit":
+                        self.player.cards.extend(self.deck.draw(1))
+                        self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id, reply_markup = other_round_markup)
+                        return
+                    case "stand":
+                        self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id, reply_markup = other_round_markup)
+                        self.player.finished = True
+                        return
+                    case _:
+                        print("unkown")
+                        return
+            elif hand_value > 21:
+                self.player.finished = True
+                self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
+                print("Bust!")
+                return
+        self.dealer.finished = True
         while self.bj_hand_value(self.dealer.cards) < 17:
             self.dealer.cards.extend(self.deck.draw(1))
+            self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
             self.print_stats()
         dealer_value = self.bj_hand_value(self.dealer.cards)
         if dealer_value > 21:
             self.print_stats()
             print("Dealer bust!")
             self.player.money += 2*bet
+            self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
+            self.bot.send_message(self.player_id, "You won!")
             return
         if dealer_value > hand_value:
             self.print_stats()
             print("Dealer won!")
+            self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
+            self.bot.send_message(self.player_id, "You lost!")
             return
         elif dealer_value == hand_value:
             self.print_stats()
             print("Stalemate!")
+            self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
+            self.bot.send_message(self.player_id, "Stalemate")
             return
         else:
             print("You won!")
             self.print_stats()
             self.player.money += 2*bet
+            self.bot.edit_message_text(self.gen_game_edit(), self.player_id, self.game_message_id)
+            self.bot.send_message(self.player_id, "You won!")
             return
 
 
     def print_stats(self):
+        """
+        Prints both player's and dealer's stats, used for debugging
+        """
         print(self.player.name, "cards: \t\t", self.player.cards, "\t value:", self.bj_hand_value(self.player.cards))
         print(self.dealer.name, "cards: \t\t", self.dealer.cards, "\t value:", self.bj_hand_value(self.dealer.cards))
 
+    def gen_game_edit(self) -> str:
+        """
+        Generates the string of the edited message sent after each update
+        """
+        if self.dealer.finished:
+            line_1 = "<b>Dealer: </b>\n" + str(self.bj_hand_value(self.dealer.cards))
+            line_2 = " ".join(self.dealer.cards) + "\n\n"
+            line_3 = "<b>Your cards: </b>" + str(self.bj_hand_value(self.player.cards)) + "\n"
+            line_4 = " ".join(self.player.cards)
+        else:
+            line_1 = "<b>Dealer:</b>\n"
+            line_2 = self.dealer.cards[0] + " ??" + "\n\n"
+            line_3 = "<b>Your cards: </b>" + str(self.bj_hand_value(self.player.cards)) + "\n"
+            line_4 = " ".join(self.player.cards)
+        return "".join([line_1, line_2, line_3, line_4])
 
     def reset_table(self) -> None:
+        """
+        Resets the table after the round is over
+        """
         self.deck.out.extend(self.player.cards)
         self.deck.out.extend(self.dealer.cards)
         self.player.cards, self.dealer.cards = [], []
@@ -101,11 +155,17 @@ class BlackJackGame:
 
 
     def shuffle_deck(self, thres: int) -> None:
+        """
+        Shuffles the whole deck after only a certain amount of cards remained
+        """
         if len(self.deck.deck) < thres:
             self.deck.shuffle_deck()
 
 
     def bj_hand_value(self, cards: list[str]) -> int:
+        """
+        Calculates the value of a given list of cards
+        """
         value = 0
         cards = [x[1:] for x in cards]
         for card in cards:
@@ -131,7 +191,7 @@ class BlackJackGame:
 
 
     def game_loop(self) -> bool:
-        self.black_jack_round()
+        self.update_black_jack_round()
         self.reset_table()
         self.shuffle_deck(65)
         return self.play_again()
--- main.py
+++ main.py
@@ -3,32 +3,53 @@ import telebot
 from dotenv import load_dotenv
 import os
 
+current_games = {}
 
 load_dotenv(".env")
 bot = telebot.TeleBot(os.environ.get("TOKEN"), parse_mode="HTML")
 
-@bot.message_handler(commands=["start", "help"])
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
     bot.reply_to(message, "Welcome to BlackJack Bot! Type /play to start playing!")
 
 @bot.message_handler(commands=["play"])
 def start_black_jack(message):
-    deck = CardDeck(FRENCH, 6)
-    deck.shuffle_deck()
-    player = Player(str(message.from_user.id), 1000)
-    game = BlackJackGame(deck, player, bot)
-    cont = True
-    while cont == True:
-        cont = game.game_loop()
+    player_id = str(message.from_user.id)
+    if player_id not in current_games:
+        current_games[player_id] = BlackJackGame(player_id, bot)
+        bot.reply_to(message, "Let's get started!")
+        bot.send_message(player_id, "Please place your bet!")
+    else:
+        bot.reply_to(message, "You already have a game going on!")
+
+@bot.message_handler(func=lambda msg: msg.text.isnumeric())
+def handle_bet(message):
+    player_id = str(message.from_user.id)
+    amount = int(message.text)
+    if player_id in current_games:
+        game = current_games[player_id]
+        if game.game_state == "BET":
+            if amount > 0 and amount < game.player.money:
+                game.player.money -= amount
+                bot.reply_to(message, "You've bet: " + message.text + " coins")
+                game.game_state = "FIRST_ROUND"
+                game.start_black_jack_round()
+            else:
+                bot.reply_to(message, "Invalid amount")
+        else:
+            bot.reply_to(message, "You can't bet currently")
+
+@bot.callback_query_handler(func = lambda call: True)
+def handle_inline(call):
+    player_id = str(call.from_user.id)
+    action = call.data
+    print(action)
+    if player_id in current_games:
+        current_games[player_id].update_black_jack_round(action)
+    bot.answer_callback_query(call.id)
 
 def main():
     bot.infinity_polling()
-    # deck.shuffle_deck()
-    # player = Player("Roland", 1000)
-    # game = BlackJackGame(deck, player)
-    # cont = True
-    # while cont == True:
-    #     cont = game.game_loop()
 
 if __name__ == "__main__":
     main()
